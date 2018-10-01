6d5f2bdde0fed480a2562221666fc1d99bdb63ea
Iulia Avram <iuliaavram@Yunos-MacBook-Pro.local>
2018-10-01 15:19:40 +0100

    Bump version to 1.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-appmail",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     install_requires=[
         'Django>=1.11',
